a682b588ab7c672bb7422908f8a2c7dc6b87645c
User <User@DESKTOP-I87ABNR>
2019-04-05 13:24:00 -0700

    Add stayle

--- FibonacciSequrenc/FibonacciSequrenc/Program.cs
+++ FibonacciSequrenc/FibonacciSequrenc/Program.cs
@@ -7,39 +7,34 @@ using System.Numerics;
 
 namespace FibonacciSequrenc
 {
-    
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             IEnumerable<BigInteger> sequence = Fibonacci();
 
-            foreach (var item in sequence.Skip(0).Take(0))            {
-
+            foreach (var item in sequence.Skip(0).Take(0))
+            {
                 Console.WriteLine(item);
-
             }
-
         }
 
         public static IEnumerable<BigInteger> Fibonacci()
         {
-            BigInteger frste =new BigInteger(0);
+            BigInteger frste = new BigInteger(0);
             BigInteger last = new BigInteger(1);
             BigInteger res = new BigInteger(0);
 
             yield return frste;
             yield return last;
-            while(true)
-            {                
+            while (true)
+            {
                 res = frste + last;
                 frste = last;
-                last  =  res;
+                last = res;
                 yield return res;
 
-            }   
-
+            }
         }
-
     }
 }
